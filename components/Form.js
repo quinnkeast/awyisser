65c769e1b0ec3a001406f1d9b9e501b73619903f
Quinn Keast <quinn@quinnkeast.com>
2022-09-27 17:31:09 +0200

    Revisit how base images are accessed

--- components/Form.js
+++ components/Form.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import Filter from 'bad-words';
-import { badWords } from './badWords.json';
+import badWordsList from './badWords.json';
 import { breakpoints } from '../styles/tokens';
 
 const Container = styled.div`
@@ -292,7 +292,7 @@ class Form extends Component {
 		}
 
 		let filter = new Filter({ emptyList: true });
-		filter.addWords(...badWords);
+		filter.addWords(...badWordsList);
 
 		if (filter.isProfane(value)) {
 			this.setState({ profane: true });
