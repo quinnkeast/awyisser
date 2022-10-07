80c57402405346f20ca4b4470abab3d6832ba0ac
Quinn Keast <quinn@quinnkeast.com>
2022-10-07 15:59:52 +0200

    Add autoexpanding input, fix mobile view a bit

--- components/header.js
+++ components/header.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 const Header = () => (
-  <div className="container mx-auto px-4 xl:max-w-screen-xl mt-12 mb-8">
+  <div className="container mx-auto px-4 xl:max-w-screen-xl mt-12 mb-4 md:mb-8">
     <div className="flex flex-col items-center text-center">
       <Link href="/">
         <a className="hover:text-blue-600 hover:cursor-pointer">
-          <h1 className="text-6xl leading-none margin-0 text-center mb-2 lowercase">
+          <h1 className="text-4xl md:text-5xl leading-none margin-0 text-center mb-2 lowercase">
             Awyisser
           </h1>
         </a>
--- components/meta.js
+++ components/meta.js
@@ -38,6 +38,7 @@ export default function Meta() {
         content="https://awyisser.com/thumbnail.png"
         key="twitter-image"
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
