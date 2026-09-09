c1afa0280653d76031ffe51da20f548ed8b49fe1
Quinn Keast <quinn@quinnkeast.com>
2026-09-09 10:35:34 -0500

    Update form.js

--- components/form.js
+++ components/form.js
@@ -168,7 +168,7 @@ class Form extends Component {
             <img
               src="/bubble-line.png"
               className="max-w-full"
-              alt="Speech bubble underline"
+              alt="Speech bubble underline!"
             />
             <img
               src="/bird-bottom.png"
